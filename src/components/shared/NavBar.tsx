049ad0915771c2b138f3c6c57220c0a0596a6216
f4faysal <f4faysals@gmail.com>
2023-08-20 16:20:07 +0600

    Working on navbar

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -1,5 +1,58 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { IoEllipsisVerticalOutline } from "react-icons/io5";
+
 const Navbar = () => {
-  return <div>Navbar Navbar Navbar</div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className=" container mx-auto py-2 px-4">
+      <div className="flex items-center justify-between container mx-auto">
+        <div className="flex items-center">
+          <Link href="/">
+            <Image
+              src="/assets/estate-ease-logo.png"
+              alt="Logo"
+              width={100}
+              height={30}
+            />
+            {/* <a className="text-white font-bold text-lg">My App</a> */}
+          </Link>
+        </div>
+        <div className="hidden md:block space-x-4">
+          <Link href="/">Home</Link>
+          <Link href="/packages">Packages</Link>
+          <Link href="/contact">Contact</Link>
+          <Link href="/login">Login</Link>
+          <Link href="/logout">Logout</Link>
+        </div>
+        <div className="md:hidden">
+          <button
+            onClick={toggleMenu}
+            className="text-base hover:text-gray-600 focus:outline-none"
+          >
+            <IoEllipsisVerticalOutline />
+          </button>
+        </div>
+      </div>
+      {isMenuOpen && (
+        <div className="md:hidden">
+          <div className="flex flex-col space-y-4 p-4">
+            <Link href="/">Home</Link>
+            <Link href="/packages">Packages</Link>
+            <Link href="/contact">Contact</Link>
+            <Link href="/login">Login</Link>
+            <Link href="/logout">Logout</Link>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,12 @@ import RootLayout from "@/components/rootLayout/RootLayout";
 import { ReactElement } from "react";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <>
+      <p className="text-2xl bg-slate-800 text-red-50 ">Home</p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
 
 Home.getLayout = function getLayout(page: ReactElement) {
